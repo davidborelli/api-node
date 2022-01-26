d1c8761b34093586d18705ec3c3871eea2a2483f
David Borelli <david_borelli@live.com>
2022-01-26 02:42:42 -0300

    chore: add rules to exclude coverage folders

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,11 @@ export default {
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   coverageDirectory: 'coverage',
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**'
+  ],
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
