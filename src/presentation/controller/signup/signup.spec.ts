54e56f46e8fc8a2309ff1a0d8d39adf072d1a4e2
David Borelli <david_borelli@live.com>
2022-01-27 02:42:59 -0300

    refactor: move success http response to a helper method

--- src/presentation/controller/signup/signup.spec.ts
+++ src/presentation/controller/signup/signup.spec.ts
@@ -20,13 +20,12 @@ const makeEmailValidator = (): EmailValidator => {
 const makeAddAccount = (): AddAccount => {
   class AddAccountStub implements AddAccount {
     add(account: AddAccountModel): AccountModel {
-      const fakeAccount = {
+      return {
         id: 'valid_id',
         name: 'valid_name',
         email: 'valid_email@email.com',
         password: 'valid_password'
       }
-      return fakeAccount
     }
   }
 
